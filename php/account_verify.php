dc90c02c7b0ba13b938982cfdd803800c9485f81
HinanawiTS <Shikieiki_Ymxndu@outlook.com>
2023-10-23 09:09:55 +0800

    第一次

---
+++ php/account_verify.php
@@ -0,0 +1,15 @@
+<?php
+$account = $_POST['account'];
+$password = $_POST['password'];
+$admin = 'hinanawiTS';
+$ad_password = 'Loremmlel.192212';
+if($account == $admin && $password == $ad_password)
+{
+    header('Location: http://www.hinanawits.com/backstage.html');
+    exit;
+}
+else
+{
+    echo '<script>alert("账户或密码错误");</script>';
+}
+?>
